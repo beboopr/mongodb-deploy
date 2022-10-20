1aba6a23e8d7d497cecb225267837375ff61f2ef
beboopr <linnets-router.09@icloud.com>
2022-10-20 16:35:22 -0400

    update furniture

---
+++ functions/src/furniture.js
@@ -0,0 +1,55 @@
+import { ObjectId } from "mongodb";
+import dbConnect from "./dbConnect.js";
+
+export async function getAllFurniture(req, res) {
+    // connect to the database
+    const db = dbConnect()
+    // get the whole funiture collection 
+    const collection = await db.collection("furniture").find().toArray()
+        // catch any errors -> status 500
+        .catch(err => {
+            res.status(500).send(err);
+            return;
+        });
+    // send back the arrray of funiture
+    res.send(collection);
+}
+
+export async function addNewFurniture(req, res) {
+    //get new furniture from rec.body = body of the request
+    const { brand, model, type, price } = req.body
+    const newFurniture = { brand, model, type, price: Number(price) }
+    // connect to database
+    const db = dbConnect()
+    //put this new funirture into our funiture collection in our db
+    await db.collection('furniture').insertOne(newFurniture)
+    //catch errors and send with status 500
+    .catch(err => {
+        res.status(500).send(err)
+        return
+      })
+
+    // return response with 201
+    res.status(201).send({ message: 'Furniture added' })
+    res.status(201).send({ message: 'Furniture updated' })
+}
+
+// export async function findFurnitureById(req, res) {
+//     const db = dbConnect()
+//     const { search } = req.params
+//     const collection = await db.collection("furniture")
+//       .find({ model: search })
+//       .toArray()
+//     res.send(collection)}
+export async function updateFurniture(req, res) {
+const db = dbConnect()
+const {furnitureId } = req.params
+await db.collection('furniture')
+//"6351a743de509c91461525f6"
+.findOneAndUpdate ({ _id: new ObjectId(furnitureId) }, { $set: req.body })
+.catch(err => {
+    res.status(500).send(err)
+    return
+})
+res.status(202).send({message: "updated furniture"})
+  }
